f5302b0c705fc2fc0e542acb52273f0249493b65
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-29 16:57:29 +0530

    adding list 56,30,70

---
+++ p.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datastructureext
+{
+    public class p
+    {
+        public static void program()
+            {
+                Console.WriteLine("Welcome to Linked List");
+
+                LinkedList2 linkedlist = new LinkedList2();
+                linkedlist.Add(70);
+                linkedlist.Add(30);
+                linkedlist.Add(56);
+                linkedlist.Display();
+
+                Console.ReadLine();
+            }
+    }
+}
