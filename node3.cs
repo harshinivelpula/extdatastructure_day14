806b511275c88a50d2fbef297f7d809d5da49bcf
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-29 17:37:15 +0530

    create class to node3

---
+++ node3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datastructureext
+{
+    internal class node3
+    {
+        public int data;
+        public node3 next;
+        public node3(int data)
+        {
+            this.data = data;
+        }
+    }
+}
