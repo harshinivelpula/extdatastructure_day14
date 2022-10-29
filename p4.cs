5dfed010ff761f1a740ffde435240ceedaea84df
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-29 17:54:35 +0530

    delete last element

---
+++ p4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datastructureext
+{
+    public class p4
+    {
+        public static void Display()
+        {
+            Console.WriteLine("Welcome to Linked List");
+
+            LinkedList6 linkedlist = new LinkedList6();
+            linkedlist.Add(56);
+            linkedlist.Add(30);
+            linkedlist.Add(70);
+            linkedlist.RemoveLastNode();
+            linkedlist.Display();
+            Console.ReadLine();
+        }
+
+    }
+}
