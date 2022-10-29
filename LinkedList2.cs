22f7276363244e70717137eef92d88d057e03c5c
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-29 17:35:27 +0530

    insert between

---
+++ LinkedList2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datastructureext
+{
+    internal class LinkedList2
+    {
+        internal node1 head;
+        internal void Add(int data)
+        {
+            node1 node = new node1(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                node1 temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.Write("\n {0} inserted into linked list", node.data);
+        }
+        public void AddinreverseOrder(int data)
+        {
+            node1 newNode = new node1(data);
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                node1 temp = this.head;
+                head = newNode;
+                head.next = temp;
+            }
+            Console.Write("\n {0} inserted into linked list", newNode.data);
+        }
+        internal void Display()
+        {
+            Console.Write("\n Data of linked list : ");
+            node1 temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ p2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datastructureext
+{
+    internal class p2
+    {
+        public static void main(string[] args)
+        {
+                Console.WriteLine("Welcome to Linked List");
+                LinkedList4 linkedlist = new LinkedList4();
+                linkedlist.Add(56);
+
+                linkedlist.Add(70);
+                linkedlist.InsertAtParticularPosition(2, 30);
+
+                LinkedList4 linkedlist4 = new LinkedList4();
+                //list.Add(30);
+                linkedlist4.Add(56);
+                linkedlist4.Add(30);
+                linkedlist4.Add(70);
+                linkedlist4.Display();
+                Console.ReadKey();
+                linkedlist.Display();
+                Console.ReadLine();
+        }
+    }
+}
