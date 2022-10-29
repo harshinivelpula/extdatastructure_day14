158e0f990a8a1c24aa4f828ba522f537e2756553
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-29 18:58:03 +0530

    show size as 3

---
+++ p7.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datastructureext
+{
+    internal class p7
+    {
+        public static void Display()
+        {
+            Console.WriteLine("Welcome to Linked List");
+
+            LinkedList9 linkedlist = new LinkedList9();
+            linkedlist.Add(56);
+            linkedlist.Add(30);
+            linkedlist.Add(40);
+            linkedlist.Add(70);
+            linkedlist.Display();
+            int position = linkedlist.Search(40);
+            linkedlist.DeleteNodeAtParticularPosition(position);
+            Console.ReadLine();
+        }
+    }
+}
