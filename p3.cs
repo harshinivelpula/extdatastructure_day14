3328aacab0e7851abecbbc72e15764af05d6ba4a
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-29 17:45:35 +0530

    delete first element

---
+++ p3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datastructureext
+{
+    public class p3
+    {
+        public static void main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List");
+
+            LinkedList5 linkedlist = new LinkedList5();
+            linkedlist.Add(56);
+            linkedlist.Add(30);
+            linkedlist.Add(70);
+            linkedlist.RemoveFirstNode();
+            linkedlist.Display();
+            Console.ReadLine();
+        }
+    }
+}
