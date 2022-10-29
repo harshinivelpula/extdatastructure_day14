efe2b2b1ac78ab245a7b1c8b7c2ff27287c0595a
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-29 16:15:44 +0530

    add program

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace datastructureext
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List");
+
+            LinkedList1 linkedlist = new LinkedList1();
+            linkedlist.Add(56);
+            linkedlist.Add(30);
+            linkedlist.Add(70);
+            linkedlist.Display();
+
+            Console.ReadLine();
+        }
+    }
+}
