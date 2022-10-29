75fdbde28bdd085e11f8c64b1adf6b39d5986183
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-29 17:13:41 +0530

    add class to node2

---
+++ node2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datastructureext
+{
+    internal class node2
+    {
+        public int data;
+        public node2 next;
+        public node2(int data)
+        {
+            this.data = data;
+        }
+    }
+}
